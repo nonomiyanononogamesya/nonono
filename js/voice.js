46e7c6bfef45974bbe4f330da71c746337350087
野野宫ののの游戏社 <nonomiyanononogamesya@gmail.com>
2020-11-02 09:58:14 +0800

    voice

---
+++ js/voice.js
@@ -0,0 +1,11 @@
+var no = [{"jp":"萌","sc":"萌","voice":[{"jp":"ありがとうバイバイ","sc":"谢谢拜拜","file":"dao"}
+												,{"jp":"キラキラ","sc":"kirakira","file":"kirakira"}
+												,{"jp":"リップロール","sc":"liproll","file":"liproll"}
+		]},{"jp":"悲鳴","sc":"怪叫","voice":[{"jp":"あびゃー","sc":"abia","file":"abia"}
+												,{"jp":"いやいやいやいや","sc":"不要不要不要不要","file":"iya"}
+		]},{"jp":"悪口","sc":"粗口","voice":[{"jp":"くそ","sc":"可恶","file":"kuso"}
+												,{"jp":"ド畜生やな","sc":"畜生","file":"chikusyou"}
+												,{"jp":"アホやろ","sc":"是不是傻","file":"ahoyaro"}
+												,{"jp":"ドアホ","sc":"大笨蛋","file":"doaho"}
+												,{"jp":"死ねよ","sc":"去死吧","file":"shineyo"}
+		]}]
