01892c9aa3b9000e52e708544df9e72d11c7860e
野野宫ののの游戏社 <nonomiyanononogamesya@gmail.com>
2020-11-02 10:41:21 +0800

    update

--- js/voice.js
+++ js/voice.js
@@ -1,6 +1,9 @@
 var no = [{"jp":"萌","sc":"萌","voice":[{"jp":"ありがとうバイバイ","sc":"谢谢拜拜","file":"dao"}
 												,{"jp":"キラキラ","sc":"kirakira","file":"kirakira"}
 												,{"jp":"リップロール","sc":"liproll","file":"liproll"}
+												,{"jp":"キース","sc":"mua","file":"mua"}
+												,{"jp":"きゃー","sc":"呀","file":"kya"}
+												,{"jp":"ミミミ","sc":"咪咪咪","file":"mimimi"}
 		]},{"jp":"悲鳴","sc":"怪叫","voice":[{"jp":"あびゃー","sc":"abia","file":"abia"}
 												,{"jp":"いやいやいやいや","sc":"不要不要不要不要","file":"iya"}
 		]},{"jp":"悪口","sc":"粗口","voice":[{"jp":"くそ","sc":"可恶","file":"kuso"}
@@ -8,4 +11,6 @@ var no = [{"jp":"萌","sc":"萌","voice":[{"jp":"ありがとうバイバイ","s
 												,{"jp":"アホやろ","sc":"是不是傻","file":"ahoyaro"}
 												,{"jp":"ドアホ","sc":"大笨蛋","file":"doaho"}
 												,{"jp":"死ねよ","sc":"去死吧","file":"shineyo"}
+		]},{"jp":"色気","sc":"色气","voice":[{"jp":"？？？","sc":"？？？","file":"？？？"}
+												,{"jp":"！？","sc":"！？","file":"！？"}
 		]}]
